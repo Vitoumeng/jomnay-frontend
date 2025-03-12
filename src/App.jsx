7ffb9badc8973b7e076e405bb0c2bd8aaa10f4b4
MENG Vuthvitou <liltou081@gmail.com>
2025-03-12 16:04:41 +0700

    Add Redux and Setup Provider in main.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Outlet />
     </div>
   );
--- src/app/layout/RootLayout.jsx
+++ src/app/layout/RootLayout.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div>
+    <>
       RootLayout
-    </div>
-  )
-}
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
